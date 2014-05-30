41649e0250fc26670bba0af9b1ca0ba70f5c9b35
Emmanuel Arreguez <emmanuel_arreguez@yahoo.com.ar>
2014-05-30 14:19:50 -0300

    Iniciando Plataforma Colmena Labs

---
+++ controllers/usuario.py
@@ -0,0 +1,26 @@
+@auth.requires_login()
+def inicio():
+    user = db(db.auth_user.id == session.auth.user.id).select(db.auth_user.first_name,
+                                                            db.auth_user.last_name,
+                                                            db.auth_user.email).first()
+    grid = SQLFORM.grid(db.ticket,
+                        searchable=False,
+                        deletable=False,
+                        csv=False,
+                        create=True)
+    return dict(user=user, ticket=grid)
+
+def servicios():
+	return dict()
+
+def soporte():
+	grid = SQLFORM.grid(db.ticket,
+                        searchable=False,
+                        deletable=False,
+                        csv=False,
+                        create=True)
+	return dict(ticket=grid)
+
+
+def perfil():
+	return dict()
